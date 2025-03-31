435cdffdb2b176a1bdefb9b6a53ae91e5ec3b039
David Philip <philip.dave@gmail.com>
2025-03-31 10:50:14 -0400

    Debuging and showing frontier

--- cell.py
+++ cell.py
@@ -3,6 +3,7 @@ width = 30
 height = 30
 WHITE = (255, 255, 255)
 BLACK = (0,0,0)
+YELLOW = (0,255,255)
 class Cell(pygame.sprite.Sprite):
     def __init__(self,x,y,position ):
         super().__init__()
@@ -13,11 +14,21 @@ class Cell(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.centerx = x
         self.rect.centery = y
+        #position is [y,x]
         self.position = position 
         #Left, right, Top, bottom
         self.walls = [1,1,1,1]
+        self.is_front = False
+        self.is_current = False
+        self.used_neighbours = []
         #self.draw_borders()
     def draw_borders(self):
+        if self.is_current:
+            self.image.fill((255,0,0))
+        elif self.is_front:
+            self.image.fill(YELLOW)
+        else:
+            self.image.fill(WHITE)
         test = ""
         if self.walls[0] == 1:
             pygame.draw.line(self.image,BLACK,[0,0],[0,height+2],width=2)
@@ -26,11 +37,12 @@ class Cell(pygame.sprite.Sprite):
             pygame.draw.line(self.image,BLACK,[width-2,0],[width-2,height+2],width=2)
        
         if self.walls[2] == 1:
-            pygame.draw.line(self.image,BLACK,[0-2,0-2],[width,-2],width=2)
+            pygame.draw.line(self.image,BLACK,[0-2,0],[width,0],width=2)
         
         if self.walls[3] == 1:
             pygame.draw.line(self.image,BLACK,[0,height-2],[width,height-2],width=2)
-        
+    
+
 
     
         
--- create_maze.py
+++ create_maze.py
@@ -1,43 +1,71 @@
 import pygame
 from random import randint
-
+from time import sleep
 neighbour_list = {1:"top",2:"bottom",3:"left",4:"right"}
 class Maze(pygame.sprite.Sprite):
 
-    def __init__(self,initial_grid, max_size):
+    def __init__(self,initial_grid, max_size,screen):
         self.grid = initial_grid
         self.maze = []
         self.max = max_size
         self.frontier = []
-        
-
+        self.screen = screen
+        self.all_cells = pygame.sprite.Group()
+        self.set_group()
     
+    def set_group(self):
+        for i in range(self.max):
+            for k in range(self.max):
+                self.all_cells.add(self.grid[i][k])
+
+    def draw_borders(self):
+        self.screen.fill((100, 0, 150))
+        pygame.display.flip()
+        self.all_cells.empty()
+        self.set_group()
+        for cell in self.all_cells:
+            #self.screen.fill((100, 0, 150))
+            #pygame.display.flip()
+            cell.draw_borders()
+        self.all_cells.update()
+        self.all_cells.draw(self.screen)
+        pygame.display.update()
+        sleep(0.8)
+
     def get_new_frontier(self,position):
         neighbor_set = []
-        left = [position[0]-1,position[1]]
-        right = [position[0]+1,position[1]]
-        top = [position[0],position[1]-1]
-        bottom = [position[0],position[1]+1]
-        
-        if left not in self.maze and left[0]>=0 and left not in self.frontier:
+        left = [position[0],position[1]-1]
+        right = [position[0],position[1]+1]
+        top = [position[0]-1,position[1]]
+        bottom = [position[0]+1,position[1]]
+        used_neighbours = self.grid[position[0]][position[1]].used_neighbours
+        if left not in self.maze and left[1]>=0 and left not in self.frontier and left not in used_neighbours:
+            self.grid[left[1]][left[0]].is_front = True
             self.frontier.append(left)
             neighbor_set.append(left)
-        elif left[0]>=0:
+        elif left[1]>=0 and left not in used_neighbours:
+            #self.grid[left[1]][left[0]].is_front = True
             neighbor_set.append(left)
-        if right not in self.maze and right[0]<self.max-1 and right not in self.frontier:
+        if right not in self.maze and right[1]<self.max and right not in self.frontier and  right not in used_neighbours:
+            self.grid[right[1]][right[0]].is_front = True
             self.frontier.append(right)
             neighbor_set.append(right)
-        elif right[0]<self.max-1:
+        elif right[1]<self.max-1 and  right not in used_neighbours:
+            #self.grid[right[1]][right[0]].is_front = True
             neighbor_set.append(right)
-        if top not in self.maze and top[1]>=0 and top not in self.frontier:
+        if top not in self.maze and top[0]>=0 and top not in self.frontier and top not in used_neighbours:
+            self.grid[top[1]][top[0]].is_front = True
             self.frontier.append(top)
             neighbor_set.append(top)
-        elif top[1]>=0:
+        elif top[0]>=0 and top not in used_neighbours:
+            #self.grid[top[1]][top[0]].is_front = True
             neighbor_set.append(top)
-        if bottom not in self.maze and bottom[1]!=self.max-1 and bottom not in self.frontier:
+        if bottom not in self.maze and bottom[0]!=self.max and bottom not in self.frontier and bottom not in used_neighbours:
+            self.grid[bottom[1]][bottom[0]].is_front = True
             self.frontier.append(bottom)
             neighbor_set.append(bottom)
-        elif position[1]<self.max-1:
+        elif position[0]<self.max-1 and bottom not in used_neighbours:
+            #self.grid[bottom[1]][bottom[0]].is_front = True
             neighbor_set.append(bottom)
         return neighbor_set
 
@@ -46,19 +74,27 @@ class Maze(pygame.sprite.Sprite):
         return neighbours[randint(0,len(neighbours)-1)]
     
     def remove_walls(self,cell,neighbour):
+        if cell[0]==0:
+            print(self.grid[cell[0]][cell[1]].walls)
+            print(self.grid[neighbour[0]][neighbour[1]].walls)
+            print("-----------------")
         if cell[0]-1==neighbour[0]:
-            self.grid[cell[0]][cell[1]].walls[0] = 0
-            self.grid[neighbour[0]][neighbour[1]].walls[1] = 0
-        elif cell[0]+1==neighbour[0]:
-            self.grid[cell[0]][cell[1]].walls[1] = 0
-            self.grid[neighbour[0]][neighbour[1]].walls[0] = 0
-        elif cell[1]-1==neighbour[1]:
             self.grid[cell[0]][cell[1]].walls[2] = 0
             self.grid[neighbour[0]][neighbour[1]].walls[3] = 0
-        elif cell[1]+1==neighbour[1]:
+        elif cell[0]+1==neighbour[0]:
             self.grid[cell[0]][cell[1]].walls[3] = 0
             self.grid[neighbour[0]][neighbour[1]].walls[2] = 0
-        
+        elif cell[1]-1==neighbour[1]:
+            self.grid[cell[0]][cell[1]].walls[0] = 0
+            self.grid[neighbour[0]][neighbour[1]].walls[1] = 0
+        elif cell[1]+1==neighbour[1]:
+            self.grid[cell[0]][cell[1]].walls[1] = 0
+            self.grid[neighbour[0]][neighbour[1]].walls[0] = 0
+        if cell[0]==0:
+            print(self.grid[cell[0]][cell[1]].walls)
+            print(self.grid[neighbour[0]][neighbour[1]].walls)
+            print("-----------------")
+        test3 = ""
 
     def create_maze(self):
         position = [5,5]
@@ -67,15 +103,23 @@ class Maze(pygame.sprite.Sprite):
         neighbours = self.get_new_frontier(position)
         while len(self.frontier)>0:
             cell = position
+            
+            if cell in self.frontier:
+                
+                self.frontier.remove(cell)
+                self.maze.append(cell)
             neighbour = self.get_random_neighbour(neighbours)
+            self.grid[cell[1]][cell[0]].used_neighbours.append(neighbour)
             self.remove_walls(cell,neighbour)
             self.maze.append(cell)
-            if cell in self.frontier:
-                self.frontier.remove(cell)
             
-            test = self.grid[position[0]][position[1]]
+            self.draw_borders()
+            self.grid[cell[1]][cell[0]].is_front = False
+            self.grid[cell[1]][cell[0]].is_current = False
+            test = self.grid[position[1]][position[0]]
             if len(self.frontier)>0:
                 position = self.frontier[randint(0,len(self.frontier)-1)]
+                self.grid[position[1]][position[0]].is_current = True
                 neighbours = self.get_new_frontier(position)
          
         
